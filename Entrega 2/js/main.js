853e96c4f967eed0548065eed355421152b90993
AlaskaBit <alejandra.sgro@interatica.com>
2025-01-15 16:01:25 -0300

    Update repo

--- Entrega 2/js/main.js
+++ Entrega 2/js/main.js
@@ -1,117 +1,109 @@
 const container = document.getElementById("product-container");
 
-let carrito;
+let miCarrito = JSON.parse(localStorage.getItem("miCarrito")) || [];
+console.log(miCarrito);
 
-if (JSON.parse(localStorage.getItem("carrito"))) {
-    carrito = JSON.parse(localStorage.getItem("carrito"));
-} else {
-    carrito = [];
+function agregarAlCarrito(pokemon) {
+
+    if (miCarrito.some(el => el.flavorName === pokemon.flavorName)) {
+        let indexPoke = miCarrito.findIndex(el => el.flavorName === pokemon.flavorName);
+        miCarrito[indexPoke].cantidad += 1;
+    } else {
+        miCarrito.push({
+            ...pokemon,
+            cantidad: 1,
+        });
+    };
+
+    localStorage.setItem("miCarrito", JSON.stringify(miCarrito));
+
+    Toastify({
+        text: `Capturaste a ${pokemon.flavorName}`,
+        duration: 3000,
+        gravity: "bottom",
+    }).showToast();
 };
 
-const arrayProductos = [
-    {
-        id: 1,
-        nombre: "Jengibre",
-        description: "HelaDitox de Jengibre, con buenas yerbas para la panza.",
-        precioKg: 200,
-        img: "img/sabores/jengibre.jpg"
-    },
-    {
-        id: 2,
-        nombre: "Lechuga",
-        description: "HelaDitox de Lechuga, por que siempre hay que tener un sabor feo.",
-        precioKg: 200,
-        img: "img/sabores/lechuga.jpg"
-    },
-    {
-        id: 3,
-        nombre: "Arándanos",
-        description: "HelaDitox de smomthie de Arandanos, para el sistema linfático.",
-        precioKg: 200,
-        img: "img/sabores/arandanos.jpg"
-    },
-    {
-        id: 4,
-        nombre: "Pera",
-        description: "HelaDitox de Pera con miel, para las caries.",
-        precioKg: 200,
-        img: "img/sabores/pera.jpg"
-    },
-    {
-        id: 5,
-        nombre: "Banana",
-        description: "HelaDitox de Banana con Dulce de Leche, para el buen humor.",
-        precioKg: 200,
-        img: "img/sabores/banana.jpg"
-    },
-    {
-        id: 6,
-        nombre: "Frutilla",
-        description: "HelaDitox de Frutilla con crema, para vivir la buena vida por siempre.",
-        precioKg: 200,
-        img: "img/sabores/frutilla.jpg"
-    },
-];
+const loadingContainer = document.createElement("div");
+loadingContainer.className = "d-flex justify-content-center";
 
-function agregarAlCarrito(producto) {
-    carrito.push(producto);
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    alert(`Agregaste 1 Kg ($${producto.precioKg}) de HelaDitox sabor ${producto.nombre} correctamente a tu carrito`);
-};
+const loadingSpinner = document.createElement("div");
+loadingSpinner.className = "spinner-grow text-success";
+
+container.appendChild(loadingContainer);
+loadingContainer.appendChild(loadingSpinner);
 
 
-function crearCard(producto) {
-    const cardCol = document.createElement("div");
-    cardCol.className = "col-3";
+setTimeout(() => {
 
-    const card = document.createElement("div");
-    card.className = "card mb-3";
+    container.removeChild(loadingContainer);
 
-    const cardBody = document.createElement("div");
-    cardBody.className = "card-body";
+    fetch("https://jellybellywikiapi.onrender.com/api/beans")
+        .then(response => response.json())
+        .then(data => {
+            const allPokemon = data.items;
+            allPokemon.forEach((el, index) => {
+                const urlBase = el.imageUrl;
 
-    const img = document.createElement("img");
-    img.src = producto.img;
-    img.alt = producto.nombre;
-    img.className = "card-img-top";
+                const card = document.createElement("div");
+                card.className = "card-pokemon";
+
+                const nombre = document.createElement("p");
+                nombre.innerText = el.flavorName;
+
+                const imagen = document.createElement("img");
+                imagen.className = "img-pokemon";
+                imagen.src = urlBase;
+                imagen.alt = el.flavorName;
+
+                const boton = document.createElement("button");
+                boton.innerText = "Capturar";
+                boton.className = "btn btn-secondary"
+                boton.addEventListener("click", () => agregarAlCarrito(el));
+
+                card.appendChild(nombre)
+                card.appendChild(imagen)
+                card.appendChild(boton)
+
+                document.getElementById("container").appendChild(card);
+            })
+            document.getElementById("container").className = "container-pokemon";
+        })
+        .catch(err => console.error(err))
+}, 2000);
+
+function crearCarts(carrito) {
+    const modalBody = document.getElementById('modalBody');
+
+    const productCart = document.createElement("div");
+    productCart.className = "product-cart";
+
+    const image = document.createElement("img");
+    image.src = carrito.imageUrl;
+    image.alt = carrito.flavorName;
+    image.className = "product-cart-img";
 
     const title = document.createElement("h5");
-    title.innerText = producto.nombre;
-    title.className = "card-title my-3";
+    title.innerText = carrito.flavorName;
+    title.className = "product-cart-title";
 
-    const cardDescription = document.createElement("p");
-    cardDescription.innerText = `${producto.description}`;
-    cardDescription.className = "card-text";
+    const priceKg = document.createElement("p");
+    priceKg.innerText = carrito.cantidad;
+    priceKg.className = "product-cart-price";
 
-    const priceBlock1 = document.createElement("div");
-    priceBlock1.className = "price-block";
+    const deleteProduct = document.createElement("button");
+    deleteProduct.innerText = "X";
+    deleteProduct.className = "btn btn-primary delete-btn";
+    deleteProduct.addEventListener('click', (e) => localStorage.removeItem());
 
-    const priceKg = document.createElement("span");
-    priceKg.innerText = `Precio por 1 Kg - $${producto.precioKg}`;
-    priceKg.className = "card-price";
+    
 
-    const btnAddKg = document.createElement("button");
-    btnAddKg.innerText = "Comprar";
-    btnAddKg.className = "btn btn-primary btn-sm";
-    btnAddKg.addEventListener("click", () => agregarAlCarrito(producto));
-
-    cardCol.appendChild(card);
-
-    card.appendChild(cardBody);
-
-    cardBody.appendChild(img);
-    cardBody.appendChild(title);
-    cardBody.appendChild(cardDescription);
-    cardBody.appendChild(priceBlock1);
-    priceBlock1.appendChild(priceKg);
-    priceBlock1.appendChild(btnAddKg);
-
-    container.appendChild(cardCol);
-};
-
-arrayProductos.forEach(el => {
-    crearCard(el);
-});
+    modalBody.appendChild(productCart);
+    productCart.appendChild(image);
+    productCart.appendChild(title);
+    productCart.appendChild(priceKg);
+    productCart.appendChild(deleteProduct);
+}
 
 const botonCarrito = document.getElementById("btn-carrito");
 botonCarrito.addEventListener("click", () => verCarrito());
@@ -121,61 +113,65 @@ function modalUp() {
     myModal.className = "modal visible";
 }
 
-function deleteProduct(carrito) {
-    carrito.localStorage.removeItem();
-}
-
-function crearCarts(carrito) {
-    modalUp();
-    const modalBody = document.getElementById('modalBody');
-
-    const productCart = document.createElement("div");
-    productCart.className = "product-cart";
-
-    const img = document.createElement("img");
-    img.src = carrito.img;
-    img.alt = carrito.nombre;
-    img.className = "product-cart-img";
-
-    const title = document.createElement("h5");
-    title.innerText = carrito.nombre;
-    title.className = "product-cart-title";
-
-    const priceKg = document.createElement("p");
-    priceKg.innerText = `Precio por 1 Kg - $${carrito.precioKg}`;
-    priceKg.className = "product-cart-price";
-
-    const deleteProduct = document.createElement("button");
-    deleteProduct.innerText = "X";
-    deleteProduct.className = "btn btn-primary delete-btn";
-    // deleteProduct.addEventListener("click", () => deleteProduct(carrito));
-
-
-    modalBody.appendChild(productCart);
-    productCart.appendChild(img);
-    productCart.appendChild(title);
-    productCart.appendChild(priceKg);
-    productCart.appendChild(deleteProduct);
-}
-
-function mensajeVacio() {
-    const modalBody = document.getElementById('modalBody');
-    const mensajeVacio = document.createElement("h5");
-    mensajeVacio.innerText = "Su carrito está vacío.";
-    mensajeVacio.className = "text-center mensajeVacio";
-    modalBody.appendChild(mensajeVacio);
-}
-
 function verCarrito() {
-    if (carrito.length > 0) {
-        carrito.forEach(el => {
-            crearCarts(el);
-        });
-    } else {
+    if (miCarrito.length > 0) {
         modalUp();
-        mensajeVacio();
+        
+        miCarrito.forEach(el => {
+            crearCarts(el);
+            // const title = document.createElement("h5");
+            // title.innerText = el.flavorName;
+            // modalBody.appendChild(title);
+        });
+        console.log(miCarrito);
+
+        // limpiarCarrito();
+
+    } else {
+        // modalUp();
+        console.log(miCarrito);
+
+        // mensajeVacio();
     }
 };
+
+document.getElementById("btn-show-pokemon").addEventListener("click", () => {
+    console.log(miCarrito)
+});
+
+document.getElementById("btn-release-pokemon").addEventListener("click", () => {
+    if (miCarrito.length > 0) {
+        const totalPokemon = miCarrito.reduce((acc, el) => acc + el.cantidad, 0);
+        Swal.fire({
+            title: "Estás seguro de liberar tus pokemon?",
+            text: `No los vas a recuperar. Esto es irreversible y ya venís capturando ${miCarrito.length} especies diferentes de pokemon y un total de ${totalPokemon}`,
+            showDenyButton: true,
+            confirmButtonText: "Sí, segurísimo",
+            denyButtonText: `No, me arrepentí`,
+            icon: "warning"
+        }).then(result => {
+            if (result.isConfirmed) {
+                miCarrito = [];
+                localStorage.setItem("miCarrito", JSON.stringify([]));
+                Swal.fire({
+                    title: "LISTO",
+                    text: "Liberaste a todos tus pokemon",
+                    icon: "success",
+                })
+            } else {
+                Swal.fire({
+                    title: "Perfecto",
+                    text: "Si te arrepentís, acá estoy",
+                })
+            }
+        })
+    } else {
+        Toastify({
+            text: "No tenés ningún pokemon que liberar",
+            duration: 3000
+        }).showToast();
+    };
+});
 
 function modalDown() {
     const modalDown = document.getElementById('myModal');
